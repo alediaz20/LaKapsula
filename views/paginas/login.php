b52011cc295c67fe0902cf808a1f1adc70aae904
adiaz <adiaz>
2022-10-12 16:19:56 -0300

    footer

--- views/paginas/login.php
+++ views/paginas/login.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -13,50 +14,58 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Theme style -->
     <link rel="stylesheet" href="views/dist/css/adminlte.min.css">
-<link rel="stylesheet" href="css/custom.css">
+    <link rel="stylesheet" href="css/custom.css">
 </head>
-<body style="font-family: 'Jost', sans-serif;" class="bg-secondary">
-<div class="container-login100">
-<div class="wrap-login100">
-    <div class="card bg-kuality">
-        <div class="card-header text-center justify-content-center">
-            <img src="../../imgs/mainlogo2.png" alt="La Kapsula" height="50%" width="50%">
-        </div>
-        <div class="card-body">
-            <form action="ajax/login.php" method="post">
-                <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="User" name="user">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-user"></span>
-                        </div>
-                    </div>
-                </div>
-                <div class="input-group mb-3">
-                    <input type="password" class="form-control" placeholder="Password" name="password">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-key"></span>
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <div class="text-center">
-                        <button type="submit" class="btn btn-kuality">Ingresar</button>
-                    </div>
-                </div>
-            </form>
-        </div>
 
+<body style="font-family: 'Jost', sans-serif;" class="bg-secondary">
+    <div class="container-login100">
+        <div class="wrap-login100">
+            <div class="card bg-kuality">
+                <div class="card-header text-center justify-content-center">
+                    <img src="../../imgs/mainlogo2.png" alt="La Kapsula" height="50%" width="50%">
+                </div>
+                <div class="card-body">
+                    <form action="ajax/login.php" method="post">
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="User" name="user">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-user"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="input-group mb-3">
+                            <input type="password" class="form-control" placeholder="Password" name="password">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-key"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="text-center">
+                                <button type="submit" class="btn btn-kuality">Ingresar</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
     </div>
 
-</div>
-</div>
-</div>
+    <footer class="mt-5">
+        <div class="container-telas">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <p class="copyright-text">Copyright &copy; 2022 All Rights Reserved by Alejandro Diaz
+                </p>
+            </div>
+        </div>
+        </footer>
+    <script src="../../plugins/jquery/jquery.min.js"></script>
 
-<script src="../../plugins/jquery/jquery.min.js"></script>
+    <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-<script src="../../dist/js/adminlte.min.js?v=3.2.0"></script>
-
+    <script src="../../dist/js/adminlte.min.js?v=3.2.0"></script>
--- views/plantilla.php
+++ views/plantilla.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,83 +16,92 @@
   <link rel="stylesheet" href="views/dist/css/adminlte.min.css">
   <link rel="stylesheet" href="css/custom.css">
 </head>
-<body style="font-family: 'Jost', sans-serif;" class="bg-secondary">
-<?php
-  if($_SESSION['user'] == 'cavila'){
-      $paginas = ["prendas","telas","listado","editarTela","editarPrenda","login"];
-  }else{
-    $paginas = ["listado","login"];
-  }
-?>
-<div class="container-fluid mb-5">
-  <div class="nav nav-justified py-2 nav-pills bg-kuality">
-    <?php if(isset($_GET['pagina'])){ ?>
-      <!-- listado -->
-      <?php if($_GET['pagina']=="listado"){ ?>
-        <li class="nav-item bg-kuality">
-          <a class="nav-link active" href="index.php?pagina=listado">Listado de prendas</a>
-        </li>
-      <?php }else{ ?>
-        <li class="nav-item bg-kuality">
-          <a class="nav-link" href="index.php?pagina=listado">Listado de prendas</a>
-        </li>
-      <?php } ?>
 
-        <?php  if($_SESSION['user'] == 'cavila') {      
+<body style="font-family: 'Jost', sans-serif;" class="bg-secondary">
+  <?php
+  if ($_SESSION['user'] == 'cavila') {
+    $paginas = ["prendas", "telas", "listado", "editarTela", "editarPrenda", "login"];
+  } else {
+    $paginas = ["listado", "login"];
+  }
+  ?>
+  <div class="container-fluid mb-5">
+    <div class="nav nav-justified py-2 nav-pills bg-kuality">
+      <?php if (isset($_GET['pagina'])) { ?>
+        <!-- listado -->
+        <?php if ($_GET['pagina'] == "listado") { ?>
+          <li class="nav-item bg-kuality">
+            <a class="nav-link active" href="index.php?pagina=listado">Listado de prendas</a>
+          </li>
+        <?php } else { ?>
+          <li class="nav-item bg-kuality">
+            <a class="nav-link" href="index.php?pagina=listado">Listado de prendas</a>
+          </li>
+        <?php } ?>
+
+        <?php if ($_SESSION['user'] == 'cavila') {
           // prendas
-          if($_GET['pagina']=="prendas"){ ?>
+          if ($_GET['pagina'] == "prendas") { ?>
             <li class="nav-item">
               <a class="nav-link active" href="index.php?pagina=prendas">Prendas</a>
             </li>
-          <?php }else{ ?>
+          <?php } else { ?>
             <li class="nav-item">
               <a class="nav-link" href="index.php?pagina=prendas">Prendas</a>
             </li>
           <?php } ?>
           <!-- telas -->
-          <?php if($_GET['pagina']=="telas"){ ?>
+          <?php if ($_GET['pagina'] == "telas") { ?>
             <li class="nav-item">
               <a class="nav-link active" href="index.php?pagina=telas">Telas</a>
             </li>
-          <?php }else{ ?>
+          <?php } else { ?>
             <li class="nav-item">
               <a class="nav-link" href="index.php?pagina=telas">Telas</a>
             </li>
-          <?php } 
-          } 
+        <?php }
+        }
         ?>
 
-      <?php }else{ ?>
+      <?php } else { ?>
         <li class="nav-item">
           <a class="nav-link active" href="index.php?pagina=listado">Listado de prendas</a>
         </li>
-        <?php  if($_SESSION['user'] == 'cavila') {  ?>
+        <?php if ($_SESSION['user'] == 'cavila') {  ?>
           <li class="nav-item">
             <a class="nav-link" href="index.php?pagina=prendas">Prendas</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="index.php?pagina=telas">Telas</a>
           </li>
-    <?php } 
-  }
-  ?>
+      <?php }
+      }
+      ?>
+    </div>
   </div>
-</div>
 
   <div>
-      <?php
-        if(isset($_GET['pagina'])){
-          if(in_array($_GET['pagina'],$paginas)){
-            include_once("paginas/".$_GET['pagina'].".php");
-          }else{
-            include_once("paginas/404.php");
-          }
-        } else{
-          include_once("paginas/listado.php");
-        }
-      ?>
+    <?php
+    if (isset($_GET['pagina'])) {
+      if (in_array($_GET['pagina'], $paginas)) {
+        include_once("paginas/" . $_GET['pagina'] . ".php");
+      } else {
+        include_once("paginas/404.php");
+      }
+    } else {
+      include_once("paginas/listado.php");
+    }
+    ?>
   </div>
 
+  <footer class="mt-5">
+    <div class="container-telas">
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <p class="copyright-text">Copyright &copy; 2022 All Rights Reserved by Alejandro Diaz
+        </p>
+      </div>
+    </div>
+  </footer>
   <!-- REQUIRED SCRIPTS -->
   <!-- jQuery -->
   <script src="views/plugins/jquery/jquery.min.js"></script>
@@ -100,4 +110,5 @@
   <!-- AdminLTE App -->
   <script src="js/sweetalert2.all.min.js"></script>
 </body>
+
 </html>
