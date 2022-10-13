b4659300788f3be7b053f5414f4f9bdbc24a32b7
adiaz <adiaz>
2022-10-13 12:02:27 -0300

    config servidor

--- ajax/config.php
+++ ajax/config.php
@@ -7,9 +7,9 @@ const COSTO_CONFECCION = 3000.00;
 
 // Base de datos
 const DB_HOST = 'localhost';
-const DB_USER = 'root';
-const DB_PASSWORD = '';
-const DB_NAME = 'capsula';
+const DB_USER = 'id19693949_alediaz';
+const DB_PASSWORD = '123741852';
+const DB_NAME = 'id19693949_capsula';
 
 const TBL_PRENDAS = 'prendas';
 const TBL_TELAS = 'telas';
