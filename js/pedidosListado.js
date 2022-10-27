d3ebf5c0116b6236a9c0e3c7fc5a5340fc1d27e4
adiaz <adiaz>
2022-10-27 15:39:42 -0300

    css

--- js/pedidosListado.js
+++ js/pedidosListado.js
@@ -14,21 +14,21 @@ $(document).ready(function(){
         responsive: true,
         dom: 'lBfrtip',
         buttons: [
-        {
-            extend: 'excel',
-            // className: 'dt-button',
-            text: '<i class="fa-solid fa-file-excel"></i> Exportar Excel',
-            className: 'btn btn-success'
-        },
-        {
-            extend: 'pdfHtml5',
-            text: '<i class="fas fa-file-pdf"></i> Exportar PDF',
-            className: 'btn btn-danger'
-        }
-    
-    ],
+            {
+                extend: 'excel',
+                // className: 'dt-button',
+                text: '<i class="fa-solid fa-file-excel"></i> Exportar Excel',
+                className: 'btn btn-success'
+            },
+            {
+                extend: 'pdfHtml5',
+                text: '<i class="fas fa-file-pdf"></i> Exportar PDF',
+                className: 'btn btn-danger'
+            }
+        ],
         language:{
             "url": "https://cdn.datatables.net/plug-ins/1.12.1/i18n/es-ES.json"
         },
+        order: [[0, 'desc']]
     });
 });
