6807bbe1bf6182912dde16f2b0d94efedd2c789e
adiaz <adiaz>
2022-11-24 16:22:52 -0300

    includes rebritgenerator en index

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ session_start();
 require_once("ajax/config.php");
 require_once("ajax/plantillaController.php");
 require_once("model/class.kapsula.php");
+require_once("includes/common.inc.php");
 
 $plantilla = new ControllerPlantilla;
 $plantilla->cTraerPlantilla();
